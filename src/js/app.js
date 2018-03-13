f787c5ba89c5fe347e85e54618dd290504b3eff8
cchnl <37313732+cchnl@users.noreply.github.com>
2018-03-13 09:19:56 -1000

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 /* sweetScroll load */
-/*
+
 document.addEventListener("DOMContentLoaded", function () {
   const sweetScroll = new SweetScroll({/* some options */});
-*/
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-/*
   particlesJS('particles-js', {
     "particles": {
       "number": {
@@ -118,4 +116,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 }, false);
-*/
